00458cad487bf2bb2d5afcff6738b415f6f55406
AndyMated <76178200+AyeMate@users.noreply.github.com>
2024-09-30 21:47:07 +0000

    commit_message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+    domains: ["media.quack.host"],
+  },
 });
